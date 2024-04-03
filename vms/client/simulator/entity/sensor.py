72a4c31486ff92891f0d0bddb749025f108c5962
Po <polina.zhirakova@gmail.com>
2024-04-03 21:37:41 +0300

    Добавлен дочерний класс для четвертого датчика.

--- vms/client/simulator/entity/sensor.py
+++ vms/client/simulator/entity/sensor.py
@@ -52,3 +52,14 @@ class Current(Sensor):
         import math
         self.value = math.sin(self.step)
         self.step = self.step + 1
+
+class CO(Sensor):
+    step = 0
+    
+    def __init__(self,name):
+        super().__init__(name)
+        self.type = "carbon oxid"
+        
+    def generate_new_value(self):
+        self.value = self.step * 1e6
+        self.step = self.step + 0.001
